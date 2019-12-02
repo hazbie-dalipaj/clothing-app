8c431582f5f374ee14d4aa0708220b12599a17e7
Hazbie Dalipaj <hazbie.dalipaj@marinetraffic.com>
2019-12-02 14:15:35 +0200

    broken our homepage into smaller components, finished styling our menu-items components

---
+++ src/components/pages/homepage/homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './homepage.scss';
+import Directory from '../../directory/Directory';
+
+const HomePage = () => (
+    <div className = 'homepage'>
+        <Directory />
+    </div>
+)
+export default HomePage;
